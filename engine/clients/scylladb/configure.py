0c9c0dc2387cc2841368040127b4ddaa857720d2
Pawel Pery <pawel.pery@scylladb.com>
2025-01-22 13:43:08 +0100

    Improve communication with scylla-usearch service

--- engine/clients/scylladb/configure.py
+++ engine/clients/scylladb/configure.py
@@ -14,6 +14,8 @@ class ScyllaDbConfigurator(BaseConfigurator):
         self.config = get_db_config(host, connection_params)
         self.keyspace_name = self.config["keyspace_name"]
         self.data_table_name = self.config["data_table_name"]
+        self.data_summary_table_name = self.config["data_summary_table_name"]
+        self.data_index_table_name = self.config["data_index_table_name"]
         self.indexes_table_name = self.config["indexes_table_name"]
         self.queries_table_name = self.config["queries_table_name"]
 
@@ -39,6 +41,7 @@ class ScyllaDbConfigurator(BaseConfigurator):
 
     def clean(self):
         self.conn.execute(f"DROP TABLE IF EXISTS {self.keyspace_name}.{self.data_table_name};")
+        self.conn.execute(f"DROP TABLE IF EXISTS {self.keyspace_name}.{self.data_summary_table_name};")
         self.conn.execute(f"DROP TABLE IF EXISTS {self.keyspace_name}.{self.queries_table_name};")
         # TODO: uncommend after proper handling of vector types and indexes is implemented in CQL
         # As for now we cannot remove keyspace as it keeps information about indexes created in the past
@@ -77,8 +80,18 @@ class ScyllaDbConfigurator(BaseConfigurator):
             CREATE TABLE IF NOT EXISTS {self.data_table_name} (
                 id BIGINT PRIMARY KEY,
                 description TEXT,
+                processed BOOLEAN,
                 embedding LIST<FLOAT>
             );
+        """)
+        self.conn.execute(f"""
+            CREATE INDEX IF NOT EXISTS {self.data_index_table_name} ON {self.data_table_name} (processed)
+        """)
+        self.conn.execute(f"""
+            CREATE TABLE IF NOT EXISTS {self.data_summary_table_name} (
+                id INT PRIMARY KEY,
+                requested_elements_count COUNTER
+            )
         """)
         print(f"Table '{self.data_table_name}' created (if not exists) in keyspace '{self.keyspace_name}'.")
         self.conn.execute(f"""
@@ -95,11 +108,12 @@ class ScyllaDbConfigurator(BaseConfigurator):
         self.conn.execute(f"""
             CREATE TABLE IF NOT EXISTS {self.queries_table_name} (
                 id INT PRIMARY KEY,
+                index_id INT,
                 embedding LIST<FLOAT>,
                 param_ef_search INT,
                 top_results_limit INT,
                 result_computed BOOLEAN,
-                result_ids LIST<INT>,
+                result_keys LIST<BIGINT>,
                 result_scores LIST<FLOAT>
             );
         """)
